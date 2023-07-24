7498b583be8ecded72737b96a33222cb84a03056
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-07-24 06:44:44 +0500

    Set Deploy

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const port = process.env.PORT || 5000;
 const cors = require("cors");
-const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorHandler");
 
 connectDB();
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.REQUEST_ORIGIN_PRODUCTION,
-  })
-);
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/get", (req, res) => {
+  res.json({ message: "Deployed" });
+});
 
 app.use("/api/user", require("./routes/Accounts"));
 app.use("/api/candidate", require("./routes/ApplyJob"));
